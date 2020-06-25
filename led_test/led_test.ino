730fbe03e295d17d04782d61e164eb1b858754a9
omegaatt36 <omegaatt36@gmail.com>
2020-06-25 16:50:45 +0800

    add func

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -1,14 +1,33 @@
-int ledPin = 0;
+const int IR_rec = 2;
+const int LED = 13;
 
-void setup() 
+void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(IR_rec, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-  delay(100);
+  int IR_status = digitalRead(IR_rec);
+  if (IR_status == 0) { 
+    return;
+  }
+  Serial.println("start");
+  unsigned long begin_time = micros();
+  while (digitalRead(IR_rec) != 0) {
+    //
+  }
+  Serial.println("end");
+  double diff = double(micros()-begin_time);
+  Serial.print("diff:");
+  Serial.println(diff);
+  if (diff > 1000000) {
+    Serial.print(diff/1000000);
+    Serial.println("\"");
+  } else {
+    Serial.print("1\\");
+    Serial.println(round(1000000/diff));
+  }
 }
